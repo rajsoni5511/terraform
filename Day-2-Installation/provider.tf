ca84db7d019e93e5cc8007c910bee295386a5035
rajeev <rajsoni.291@gmail.com>
2025-07-22 17:59:12 +0530

    new

--- Day-2-Installation/provider.tf
+++ Day-2-Installation/provider.tf
@@ -1,12 +1,4 @@
 provider "aws" {
 
- 
-}
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0, < 5.0"
-    }
-  }
-}
+                              }
+
